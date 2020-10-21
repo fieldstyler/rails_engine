ce9a0fc3806945a813cd2f7f00118f8f172fb306
fieldstyler <tdfields93@gmail.com>
2020-10-21 13:51:03 -0600

    Complete create functionality for item response
    Co-authored-by: <Name><Email>

--- spec/requests/api/v1/merchants/items_request_spec.rb
+++ spec/requests/api/v1/merchants/items_request_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Items API Request' do
   it 'imports an item list' do
-    create(:merchant)
-    merchant = Merchant.first
-    create_list(:item, 5, merchant_id: merchant.id)
+    create_list(:item, 5)
     get '/api/v1/items'
 
     expect(response).to be_successful
@@ -32,20 +30,53 @@ RSpec.describe 'Items API Request' do
 
   end
   it 'can find item by id' do
-    create(:merchant)
-    faker_item = create(:item)
-    id = faker_item.id
+    id = create(:item).id
 
     get "/api/v1/items/#{id}"
 
     expect(response).to be_successful
-    expect(faker_item).to have_key(:id)
-    expect(faker_item.id).to be_an Integer
-    expect(faker_item.name).to be_a String
-    expect(faker_item.description).to be_a String
-    expect(faker_item.unit_price).to be_a Float
-    expect(faker_item.merchant_id).to be_an Integer
-    expect(faker_item.created_at).to be_a Time
-    expect(faker_item.updated_at).to be_a Time
+    item_data = JSON.parse(response.body, symbolize_names: true)
+    item = item_data[:data]
+
+    expect(item).to have_key(:id)
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a String
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a String
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a Float
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_an Integer
+    expect(item[:attributes]).to have_key(:created_at)
+    expect(item[:attributes][:created_at]).to be_a String
+    expect(item[:attributes]).to have_key(:updated_at)
+    expect(item[:attributes][:updated_at]).to be_a String
+  end
+
+  it 'can create an item record' do
+    merchant = create(:merchant)
+
+    item_params = ({
+      name: 'Sad Strange Chamaleon',
+      id: '1',
+      description: 'Like a normal chamaleon, but sad and strange',
+      unit_price: 324.24,
+      merchant_id: merchant.id,
+      created_at: '04-06-2020 12:16:17 UTC',
+      updated_at: '04-06-2020 12:16:17 UTC'
+      })
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v1/items", headers: headers, params: JSON.generate(item_params)
+      created_item = Item.last
+      expect(response).to be_successful
+
+      expect(created_item.name).to eq(item_params[:name])
+      expect(created_item.description).to eq(item_params[:description])
+      expect(created_item.unit_price).to eq(item_params[:unit_price])
+      expect(created_item.created_at).to eq(item_params[:created_at])
+      expect(created_item.updated_at).to eq(item_params[:updated_at])
+      expect(created_item.merchant_id).to eq(item_params[:merchant_id])
   end
 end
